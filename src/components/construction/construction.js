5ab0b5649fbd18035f059056e5b6581fe581f3a0
Candela Alfano <candela.alfano1503@gmail.com>
2024-07-01 01:10:23 -0300

    change for mobile

--- src/components/construction/construction.js
+++ src/components/construction/construction.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import './construction.scss'
 import construction from '../../img/building-page.png'
+import { useMediaQuery } from 'react-responsive';
 
 const Construction = () => {
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 840px)' });
+
+
     return (
         <main className='main-home'>
             <img className='img-in-construction' src={construction} alt='*'></img>
-            <p className='p1'>⚠️ página en construcción ⚠️</p>
+            {
+            isTabletOrMobile?
+                <>
+                <p className='p1'>⚠️</p>
+                <p className='p1'>página en</p>
+                <p className='p1'>construcción</p>
+                </>
+            :
+                <p className='p1'>⚠️ página en construcción ⚠️</p>
+            }
             <div className='div-contact'>
                 <p className='p2'>Contáctanos a nuestro e-mail: <a className='a-contact' href='mailto:bedigitalservicios@gmail.com'>bedigitalservicios@gmail.com</a></p>
             </div>
